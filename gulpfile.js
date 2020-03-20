bde4fa610a09dbb0e2bf3ae6562707c399a97491
Evgeniy137 <vavilon11111111@gmail.com>
2020-03-20 14:15:33 +0200

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,51 @@
+let gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    uglify = require('gulp-uglify'),
+    concat = require('gulp-concat'),
+    uglifycss = require('gulp-uglifycss');
+    autoprefixer = require('gulp-autoprefixer');
+
+
+
+
+
+gulp.task('sass', function () {
+    return gulp.src('app/scss/*.scss')
+        .pipe(sass({outputStyle: 'expanded' }))
+        .pipe(autoprefixer())
+        .pipe(gulp.dest('app/css'));
+});
+
+gulp.task('js', function(){
+    return gulp.src([
+    ])
+    .pipe(concat('libs.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest("app/js"))
+})
+
+gulp.task('scripts', function(){
+    return gulp.src('app/js/scripts/*.js')
+    .pipe(concat('scripts.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest("app/js"))
+})
+
+gulp.task('min-css',function(){
+    return gulp.src('app/css/style.css')
+    .pipe(concat('css.min.css'))
+    .pipe(uglifycss())
+    .pipe(gulp.dest("app/css"))
+})
+
+gulp.task('autoprefixer', function(){
+    return gulp.src('app/css/style.css')
+    .pipe(autoprefixer())
+})
+
+
+gulp.task('watch', function() {
+    gulp.watch('app/scss/*.scss', gulp.parallel('sass'));
+    gulp.watch('app/js/scripts/*js', gulp.parallel('scripts'))
+    gulp.watch('app/css/style.css',gulp.parallel('min-css') )
+});
